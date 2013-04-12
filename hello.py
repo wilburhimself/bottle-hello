5539abfca62ff61eafe0e66032c2fa0834c19395
Wilbur Suero <wilbur.himself@gmail.com>
2013-04-12 11:44:37 -0400

    Hello :name

---
+++ hello.py
@@ -0,0 +1,6 @@
+from bottle import run, route, template
+@route('/hello/:name')
+def index(name="World"):
+    return template('<b>Hello {{name}}</b>', name=name)
+
+run(host='localhost', port=8080)
